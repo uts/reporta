9bc6445b0c12329c2e409d43fd038f2d7bd5d90f
Garrett Heinlen <heinleng@gmail.com>
2013-06-22 13:15:16 +1000

    fixing filter specs.

--- spec/models/filter_spec.rb
+++ spec/models/filter_spec.rb
@@ -2,52 +2,42 @@ require 'spec_helper'
 
 class DummyReport
   include Reporta::Filter
-
-  filter :start_date, default: '2013-01-01'
-  filter :finish_date, default: '2013-12-13'
 end
 
 describe Reporta::Filter do
-  it 'can be initialized' do
-    expect { DummyReport.new }.to_not raise_error
+  it 'initializes filters' do
+    expect { DummyReport.filter :start_date }.to_not raise_error
   end
 
-  context 'with filter containing all available options' do
-    before(:each) do
-      DummyReport.filter(:start_date, default: '2013-01-01')
-      DummyReport.filter(:finish_date, default: (Date.today + 5.days))
+  context 'with columns defined' do
+    before :each do
+      DummyReport.filter :start_date
       @report = DummyReport.new
     end
 
-    it 'returns the default value when none is supplied' do
-      expect(@report.start_date).to eq '2013-01-01'
+    it 'iterates over filters' do
+      expect(@report.filters.length).to eq 1
     end
 
-    it 'allows the defaults to be overridden' do
-      expect(@report.start_date).to eq '2013-01-01'
+    it 'defaults to the filter name' do
+      DummyReport.filter(:start_date, {default: '2013-01-01'})
+      report = DummyReport.new
+      expect(report.filters[:start_date].default).to eq '2013-01-01'
     end
 
-    it 'provides a way to iterate over each filter and retrieve the name and options' do
-      expect(@report.filters.size).to eq 2
-      filter = @report.filters.first
-      expect(filter.name).to eq :start_date
-      expect(filter.options).to eq({ :default => '2013-01-01' })
+    it 'sets a custom title' do
+      DummyReport.filter :start_date, required: true
+      report = DummyReport.new
+      expect(report.filters[:start_date].required).to eq true
     end
+
+    it 'sets custom class names' do
+      DummyReport.filter :start_date, as: :boolean
+      report = DummyReport.new
+      expect(report.filters[:start_date].as).to eq :boolean
+    end
+
+    it "collection - setting a collection will force the filter to render as a select input."
+    it "include_blank - only has an affect if a collection is set. defaults to true."
   end
-
-  context "Validations" do
-    it 'raises an error if name is blank' do
-      DummyReport.filter(:start_date, required: true)
-      abc = DummyReport.new
-      expect(abc.errors[:start_date]).to eq "Can't be blank"
-    end
-  end
-
-  it "required - set to true to force a field to be set. Defaults to false."
-  it "default - set the default value for the filter."
-  
-  it "collection - setting a collection will force the filter to render as a select input."
-  it "include_blank - only has an affect if a collection is set. defaults to true."
-  it "as - set the type of field to render. Available values are :boolean, :string, :check_boxes, :radio. Defaults to :string."
-
 end
