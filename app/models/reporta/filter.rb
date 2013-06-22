762d5a2d6ecd278fabd59d78605d09dd58b39c85
Garrett Heinlen <heinleng@gmail.com>
2013-06-22 17:13:53 +1000

    Adding a private method to set form values.

--- app/models/reporta/filter.rb
+++ app/models/reporta/filter.rb
@@ -15,27 +15,44 @@ module Reporta
           include_blank: options[:include_blank].nil?,
           name: options[:name].to_s.humanize
         })
-        report_filter = OpenStruct.new(options)
-        all_filters[name] = report_filter
+
+        all_filters[name] = OpenStruct.new(options)
       end
     end
 
     def initialize(args={})
       args ||= {}
       @form = Reporta::Form.new all_filters
-      all_filters.each do |name, value|
-        val = if args[name].present?
-          args[name]
-        else
-          all_filters[name.to_sym].default
-        end
-        form.send "#{name}=", val
-      end
+      set_form_values(args)
     end
 
     def filters
       all_filters
     end
 
+    private
+    def set_form_values(args)
+      all_filters.each do |name, value|
+        val = if args[name].present?
+          args[name]
+        else
+          all_filters[name.to_sym].default
+        end
+
+        if all_filters[name.to_sym].as == :boolean
+          if val == "0"
+            val = false
+          elsif val == "1"
+            val = true
+          end
+        end
+
+        form.send "#{name}=", val
+      end
+    end
+
+    def method_missing(*args, &block)
+      self.form.send(*args, &block)
+    end
   end
 end
