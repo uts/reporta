cf5b44658d542d622f6cf727e402845aa0917b92
Scott Harvey <scottandrewharvey@gmail.com>
2013-06-23 19:15:09 +1000

    Moved form setup into the Report class

--- app/models/reporta/filter.rb
+++ app/models/reporta/filter.rb
@@ -4,8 +4,6 @@ module Reporta
   module Filter
     extend ActiveSupport::Concern
 
-    attr_accessor :form
-
     included do
       cattr_accessor :filters
       self.filters = {}
@@ -21,32 +19,7 @@ module Reporta
       end
     end
 
-    def initialize(args={})
-      args ||= {}
-      @form = Reporta::Form.new filters
-      set_form_values(args)
-      form.valid? if args.present?
-    end
-
     private
-
-    def set_form_values(args)
-      filters.each do |name, value|
-        value = filter_value(args, name)
-        value = convert_boolean(name, value) if filters[name].boolean?
-
-        form.send "#{name}=", value
-      end
-    end
-
-    # TODO: Please fix me.
-    def convert_boolean(name, value)
-      if value == "0"
-        false
-      elsif value == "1"
-        true
-      end
-    end
 
     def filter_value(args, name)
       val = if args[name].present?
@@ -54,10 +27,6 @@ module Reporta
       else
         filters[name.to_sym].default
       end
-    end
-
-    def method_missing(*args, &block)
-      self.form.send(*args, &block)
     end
   end
 end
--- app/models/reporta/report.rb
+++ app/models/reporta/report.rb
@@ -3,5 +3,38 @@ module Reporta
     extend ActiveSupport::Concern
     include Filter
     include Column
+
+    attr_accessor :form
+
+    def initialize(args={})
+      args ||= {}
+      @form = Reporta::Form.new filters
+      set_form_values(args)
+      form.valid? if args.present?
+    end
+
+    private
+
+    def set_form_values(args)
+      filters.each do |name, value|
+        value = filter_value(args, name)
+        value = convert_boolean(name, value) if filters[name].boolean?
+
+        form.send "#{name}=", value
+      end
+    end
+
+    # TODO: Please fix me.
+    def convert_boolean(name, value)
+      if value == "0"
+        false
+      elsif value == "1"
+        true
+      end
+    end
+
+    def method_missing(*args, &block)
+      self.form.send(*args, &block)
+    end
   end
 end
