0b8f13e56bf0ef3b0a1d85efd8b4d01c12e0def2
Scott Harvey <scottandrewharvey@gmail.com>
2013-06-23 16:32:38 +1000

    Removed last_name filter

--- spec/dummy/app/models/project_report.rb
+++ spec/dummy/app/models/project_report.rb
@@ -2,7 +2,6 @@ class ProjectReport
   include Reporta::Report
 
   filter :first_name, default: "Garrett"
-  filter :last_name
   filter :age, required: true
   filter :gender, collection: %w/MALE FEMALE/, include_blank: false
   filter :active, as: :boolean
