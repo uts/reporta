e318b8fda6e948fc6624dba4728f627f37794e20
Scott Harvey <scottandrewharvey@gmail.com>
2013-09-01 12:15:52 +0700

    Initial setup to get dynamic tables working

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -10,4 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require_tree .
+//= require jquery
+//= require jquery.dataTables
+//= require reporta/dataTables
