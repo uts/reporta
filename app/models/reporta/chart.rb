6009912a580b08deadf51f4d3d0a51bcca1386f7
Scott Harvey <scottandrewharvey@gmail.com>
2013-09-03 06:45:39 +0700

    Implemented the x_axis and y_axis class methods

--- app/models/reporta/chart.rb
+++ app/models/reporta/chart.rb
@@ -1,4 +1,37 @@
+require 'ostruct'
+
 module Reporta
   module Chart
+    extend ActiveSupport::Concern
+
+    included do
+      cattr_accessor :x_axis_options, :y_axis_options
+    end
+
+    module ClassMethods
+      def x_axis(name, options={})
+        title = (options[:title] || name).to_s.humanize
+        self.x_axis_options = OpenStruct.new options.reverse_merge(title: title)
+      end
+
+      def y_axis(name, options={})
+        title = (options[:title] || name).to_s.humanize
+        self.y_axis_options = OpenStruct.new options.reverse_merge(title: title)
+      end
+
+      def line_chart(name, options={})
+      end
+
+      def column_chart(name, options={})
+      end
+    end
+
+    def x_axis
+      x_axis_options
+    end
+
+    def y_axis
+      y_axis_options
+    end
   end
 end
--- spec/models/chart_spec.rb
+++ spec/models/chart_spec.rb
@@ -4,18 +4,31 @@ class ChartReport
   include Reporta::Chart
 
   x_axis :months
-  y_axis :accounts
+  y_axis :accounts, title: 'Total Accounts'
 
-  line :sign_ups
-  column :average_age
+  line_chart :sign_ups
+  column_chart :average_age
 
   def months
     @months ||= (1..12).map { |month| Date.new(Date.today.year, month) }
   end
 
-  def sign_ups
-    Account.all.group_by do |account|
-      account.created_at.beginning_of_month
+  def sign_ups(month)
+    accounts_by_month[month].size
+  end
+
+  def average_age(month)
+    accounts = accounts_by_month[month]
+    accounts.sum(&:age) / accounts.size
+  end
+
+  private
+
+  def accounts_by_month
+    @accounts_by_month ||= begin
+      Account.all.group_by do |account|
+        account.created_at.beginning_of_month
+      end
     end
   end
 
@@ -24,5 +37,11 @@ end
 describe Reporta::Chart do
   it 'can be initialized' do
     expect { ChartReport.new }.not_to raise_error
+  end
+
+  it 'has an x and y axis' do
+    report = ChartReport.new
+    expect(report.x_axis.title).to eq 'Months'
+    expect(report.y_axis.title).to eq 'Total Accounts'
   end
 end
