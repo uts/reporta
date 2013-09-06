1411da4fd00847f017d8749561a6dd1b119ef4d6
Scott Harvey <scottandrewharvey@gmail.com>
2013-09-06 07:28:23 +0700

    Fixed flot.js permissions and created overview page

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: "reports#table"
-  match '/', to: 'reports#table', via: :post
+  root to: "reports#overview"
 
   %w(table dynamic_table dynamic_table_with_bootstrap chart).each do |action|
     match "/#{action}", to: "reports##{action}", via: [:get, :post]
