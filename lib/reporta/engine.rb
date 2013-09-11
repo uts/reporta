eeb4a2d03884f352059f52c3e7ec72ef5b463baa
Garrett Heinlen <heinleng@gmail.com>
2013-09-11 13:06:06 +1000

    adding an initializer to load the helpers automatically in rails apps.

--- lib/reporta/engine.rb
+++ lib/reporta/engine.rb
@@ -1,5 +1,12 @@
 module Reporta
   class Engine < ::Rails::Engine
     isolate_namespace Reporta
+
+    initializer 'reporta.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper Reporta::Engine.helpers
+      end
+    end
+
   end
 end
