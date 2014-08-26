ea102d63b0ce02b315f58b9c6de99c4f1381f6ac
alvinsj <alvin.nsj@gmail.com>
2014-08-26 14:16:21 +0700

    fix spec

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -18,9 +18,9 @@ describe Reporta::Form do
 
   it 'stores and retreives values based on the filter name' do
     form = Reporta::Form.new filters
-    expect(form.active).to be_false
+    expect(form.active).to be false
     form.active = true
-    expect(form.active).to be_true
+    expect(form.active).to be true
   end
 
   it 'falls back to the filter default value if set' do
@@ -30,31 +30,31 @@ describe Reporta::Form do
 
   it 'adds an error message on validation when a required field is missing' do
     form = Reporta::Form.new filters
-    expect(form.valid?).to be_false
+    expect(form.valid?).to be false
     expect(form.errors[:name].to_s).to match /required/
   end
 
   describe 'filters_applied?' do
     it 'is false when no values are initialized' do
       form = Reporta::Form.new filters
-      expect(form.filter_applied?).to be_false
+      expect(form.filter_applied?).to be false
     end
 
     it 'is true when one or more values are initialized' do
       form = Reporta::Form.new filters, active: true
-      expect(form.filter_applied?).to be_true
+      expect(form.filter_applied?).to be true
     end
   end
 
   context 'with boolean filter' do
     it 'converts boolean "0" to false' do
       form = Reporta::Form.new filters, active: '0'
-      expect(form.active).to be_false
+      expect(form.active).to be false
     end
 
     it 'converts boolean "1" to true' do
       form = Reporta::Form.new filters, active: '1'
-      expect(form.active).to be_true
+      expect(form.active).to be true
     end
   end
 
